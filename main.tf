c6dd2eb3bb0ee7182f261577a873433c919e54eb
timdeluxe <5765175+timdeluxe@users.noreply.github.com>
2021-08-18 11:11:05 +0200

    feat(DO-779): Removing blob (terraform manages it by itself) and improving storage account (mostly security)

--- main.tf
+++ main.tf
@@ -1,26 +1,23 @@
-# Azure Storage blob for state storage
-
 resource "azurerm_storage_account" "storage-account" {
   name                      = "${lower(var.project)}${lower(var.stage)}stateacc"
   resource_group_name       = var.resource_group
   location                  = var.location
+  account_kind              = "StorageV2"
   account_tier              = "Standard"
   account_replication_type  = "ZRS"
-  enable_https_traffic_only = false
+  enable_https_traffic_only = true
+  allow_blob_public_access  = false
+  min_tls_version           = "TLS1_2"
+
+  blob_properties {
+    delete_retention_policy {
+      days = var.delete_retention_policy
+    }
+  }
 }
 
 resource "azurerm_storage_container" "storage-container" {
   name                  = "${lower(var.project)}${lower(var.stage)}statecont"
   storage_account_name  = azurerm_storage_account.storage-account.name
   container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "storage-blob" {
-  name = "${lower(var.project)}${lower(var.stage)}stateblob"
-
-  storage_account_name   = azurerm_storage_account.storage-account.name
-  storage_container_name = azurerm_storage_container.storage-container.name
-
-  type = "Block"
-  size = var.size
 }
