0150eedcc880bbc8464e0a901d318234c4a5b57a
Dennis Ploeger <develop@dieploegers.de>
2022-04-26 11:16:01 +0200

    fix: Support ARM >=3
    BREAKING CHANGE: Renames parameter from ARM provider 2 to 3

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "azurerm_storage_account" "storage-account" {
-  name                      = "${lower(var.project)}${lower(var.stage)}stateacc"
-  resource_group_name       = var.resource_group
-  location                  = var.location
-  account_kind              = "StorageV2"
-  account_tier              = "Standard"
-  account_replication_type  = "ZRS"
-  enable_https_traffic_only = true
-  allow_blob_public_access  = false
-  min_tls_version           = "TLS1_2"
-  tags                      = var.tags
+  name                            = "${lower(var.project)}${lower(var.stage)}stateacc"
+  resource_group_name             = var.resource_group
+  location                        = var.location
+  account_kind                    = "StorageV2"
+  account_tier                    = "Standard"
+  account_replication_type        = "ZRS"
+  enable_https_traffic_only       = true
+  allow_nested_items_to_be_public = false
+  min_tls_version                 = "TLS1_2"
+  tags                            = var.tags
 
   blob_properties {
     delete_retention_policy {
